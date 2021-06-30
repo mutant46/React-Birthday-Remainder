44bc5065c2520e183bc5c5322efdb082f2865cdb
junai <lucifermjm@gmail.com>
2021-06-30 20:20:44 +0500

    Initial Commits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+// data
+import { data } from "./data";
+
+// global varibale
+var wlist = [];
+
+const Layout = () => {
+  const [people, setPeople] = useState(data);
+  const [wished, setWished] = useState([]);
+
+  const markall = () => {
+    setPeople([]);
+    setWished(data);
+  };
+
+  const removePerson = (id) => {
+    const list = people.filter((p) => p.id !== id);
+
+    for (var x of people) {
+      if (x.id === id) {
+        wlist.push(x);
+        break;
+      }
+    }
+
+    setPeople(list);
+    setWished(wlist);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <React.Fragment>
+      <div className="list-container">
+        <section>
+          <h2>{people.length} wishes left</h2>
+          {people.map((person) => {
+            const { profile, name, age, id } = person;
+            return (
+              <div className="person-wrapper" key={id}>
+                <div className="info">
+                  <img src={profile} alt="" />
+                  <div className="id">
+                    <h3>{name}</h3>
+                    <span>{age} years</span>
+                  </div>
+                </div>
+                <button type="button" onClick={() => removePerson(id)}>
+                  Mark Wished
+                </button>
+              </div>
+            );
+          })}
 
-export default App;
+          <button id="clear" type="button" onClick={markall}>
+            mark all
+          </button>
+        </section>
+        <section>
+          <h2>{wished.length} wished</h2>
+          {wished.map((person) => {
+            const { profile, name, age, id } = person;
+            return (
+              <div className="person-wrapper" key={id}>
+                <div className="info">
+                  <img src={profile} alt="" />
+                  <div className="id">
+                    <h3>{name}</h3>
+                    <span>{age} years</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Layout;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import reactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// css files
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// apps
+import App from "./App";
+
+const Main = () => {
+  return (
+    <main>
+      <h1 style={{ textAlign: "center", letterSpacing: "2px" }}>
+        BIRTHDAY REMAINDER
+      </h1>
+      <App />
+    </main>
+  );
+};
+
+reactDom.render(<Main />, document.getElementById("root"));
