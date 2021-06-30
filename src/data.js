a803a4c7bcfae3cc12793abf5144aed00b564e60
junai <lucifermjm@gmail.com>
2021-06-30 20:24:38 +0500

    added media and json data:

---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const data = [{
+        id: 1,
+        profile: 'hanan.jfif',
+        name: 'Ali Hanan',
+        age: '19 years'
+    },
+    {
+        id: 2,
+        profile: 'ali.jfif',
+        name: 'Muhammad Ali',
+        age: '29 years'
+
+    }, {
+        id: 3,
+        profile: 'hester.png',
+        name: 'Hester Little',
+        age: '36 years'
+
+    }, {
+        id: 4,
+        profile: 'sean.jfif',
+        name: 'Sean Walsh',
+        age: '34 years'
+
+    }, {
+        id: 5,
+        profile: 'lola.png',
+        name: 'Lola Gardner',
+        age: '26 years'
+    }
+]
